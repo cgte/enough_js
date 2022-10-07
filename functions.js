b2793a0af9994d58e674924ebdda834eed12b669
Colin Goutte <cgte@bk.ru>
2022-10-07 13:54:17 +0200

    Add new function:

---
+++ functions.js
@@ -0,0 +1,47 @@
+
+let _=console.log;
+
+function stringMessage(string) {
+  if(string){
+    return "The string is nonempty" ;
+  }
+  else {
+    return "The string is empty";
+  }
+
+}
+
+_(stringMessage());
+_(stringMessage(''));
+_(stringMessage('Colin'));
+
+
+function number_compare(a, b){
+  if (a>b){return 1}
+  else if (a==b || a === b) {return 0}
+  else if (a <b ) {return -1}
+}
+
+let a= [8, 17 ,42, 99];
+
+
+_(a.sort())
+_(a.sort(number_compare))
+
+
+function square(number) {return number * number;}
+
+_(square(2))
+
+let fatsquare = (number) => {return number ** 2}
+
+_(fatsquare(2))
+
+for (let i=-10; i <= 10;  i ++){
+  let slim = square(i);
+  let fat = fatsquare(i);
+  if (!(slim == fat)) {_(`test failed for ${slim} ${fat}`)}
+  if (!slim == fat) {_(`test failed for ${slim} ${fat}`)}
+  if (slim != fat) {_(`test failed for ${slim} ${fat}`)}
+}
+
