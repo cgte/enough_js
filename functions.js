8c609c503d22038b4f42329f949fe7434bc33ab8
Colin Goutte <cgte@bk.ru>
2022-10-07 14:25:59 +0200

    Add palindrome code

--- functions.js
+++ functions.js
@@ -61,4 +61,34 @@ _(dayname(new Date()) );
 
 _(dayname());
 
+
+
+_("Chainning")
+
+let reverse1 = (string)=> {
+  let letters = string.split();
+  letters.reverse();
+  return letters.join('');
+}
+
+let reverse2 = (string) => {
+  // using method chaining
+  return string.split().reverse().join('') ;
+}
+
+let palindrome = (string)=> {
+  return string === reverse1(string);
+}
+
+
+_(palindrome('radar'))
+
+
+
+
+
 console.log("functions module loaded");
+
+
+
+
